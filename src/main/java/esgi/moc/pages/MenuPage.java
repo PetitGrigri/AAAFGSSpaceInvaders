fa59bacf594fc020b4683300cf810b8d0e31a7fe
Asifadam93 <adamsha.asif@gmail.com>
2017-06-06 16:30:47 +0200

    activity added !

--- src/main/java/esgi/moc/pages/MenuPage.java
+++ src/main/java/esgi/moc/pages/MenuPage.java
@@ -1,5 +1,35 @@
 package esgi.moc.pages;
 
-public class MenuPage {
+import ej.widget.composed.Button;
+import ej.widget.container.List;
+import ej.widget.navigation.page.Page;
+
+public class MenuPage extends Page {
+	
+	private static List list;
+	
+	public MenuPage(){
+		
+		list = new List(true);
+
+		Button boardButton = new Button("Play !");
+		boardButton.addClassSelector("BUTTON");
+		boardButton.getLabel().addClassSelector("LABEL");
+		
+		Button scoreButton = new Button("Scores");
+		scoreButton.addClassSelector("BUTTON");
+		scoreButton.getLabel().addClassSelector("LABEL");
+		
+		Button exitButton = new Button("Exit");
+		exitButton.addClassSelector("BUTTON");
+		exitButton.getLabel().addClassSelector("LABEL");
+		
+		list.add(boardButton);
+		list.add(scoreButton);
+		list.add(exitButton);
+		
+		setWidget(list);
+		
+	}
 	
 }
